19855fe62ab2f34c4b37c9bd2e57f7726986fe24
Sam Nicknam <s.nicknam@icloud.com>
2018-02-11 20:27:05 +0100

    fixed missing '*' in header

--- Linkedlist/llist.h
+++ Linkedlist/llist.h
@@ -10,7 +10,7 @@ void llist_add_i(Node** llist, int index, void* data);
 void llist_addAll(Node** llist, Node* other);
 void llist_addAll_i(Node** llist, int index, Node* other);
 void llist_remove_i(Node** llist, int index);
-void llist_remove_e(Node* llist, void* data);
+void llist_remove_e(Node** llist, void* data);
 void llist_print(Node* node, printer printer);
 
 struct _Node {
